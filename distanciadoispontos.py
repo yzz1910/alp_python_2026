0ef688f3eb8a2646249427a5b7b983cf61664a40
yzz1910 <yuusccp@gmail.com>
2026-04-16 11:12:23 -0300

    casas decimais reduzidas em duas

--- distanciadoispontos.py
+++ distanciadoispontos.py
@@ -8,4 +8,4 @@ y2 = float(input("digite o valor de y2: "))
 
 distancia = sqrt((x2-x1)**2 + (y2-y1)**2)
 
-print(f"a distância entre os dois pontos é: {distancia}")
+print(f"a distância entre os dois pontos é: {distancia:.2f}")

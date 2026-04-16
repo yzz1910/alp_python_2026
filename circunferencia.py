86a35f2b8f070dd70fc03f5fa40f1d9c7a9625f8
yzz1910 <yuusccp@gmail.com>
2026-04-16 10:46:08 -0300

    arquivos adicionados: circunferencia.py, distanciadoispontos.py e imc.py

---
+++ circunferencia.py
@@ -0,0 +1,6 @@
+from math import pi
+
+raio = int(input("digite o valor do raio da circunferência: "))
+comprimento = 2 * pi * raio
+
+print(f"o comprimento da circunferência é: {comprimento}")
---
+++ distanciadoispontos.py
@@ -0,0 +1,11 @@
+from math import sqrt
+
+x1 = float(input("digite o valor de x1: "))
+y1 = float(input("digite o valor de y1: "))
+
+x2 = float(input("digite o valor de x2: "))
+y2 = float(input("digite o valor de y2: "))
+
+distancia = sqrt((x2-x1)**2 + (y2-y1)**2)
+
+print(f"a distância entre os dois pontos é: {distancia}")

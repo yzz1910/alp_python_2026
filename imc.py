287e92528f4319b0d51735cf5c795f7692fd2a12
yzz1910 <yuusccp@gmail.comgit config --global user.name yzz1910git config --global user.name yuusccp@gmail.com>
2026-04-17 12:01:48 -0300

    calculadora imc atualizada

--- imc.py
+++ imc.py
@@ -1,6 +1,26 @@
+#calculadora imc
+print("""
+      ----------- Tabela de IMC -----------
+
+menor que 18.5       | magreza
+entre 18.5 e 24.9    | peso normal
+entre 25.0 e 29.9    | sobrepeso
+entre 30.0 e 39.9    | obesidade
+acima de 40          | obesidade grave
+------------------------------------
+""")
+
 peso = float(input("digite o seu peso: "))
 altura = float(input("digite a sua altura: "))
-
 imc = peso/(altura**2)
 
-print(f"o valor do IMC é: {imc:.2f}")
+if imc < 18.5:
+    print("Magreza")
+elif 18.5 <= imc <= 24.9:
+    print(f"peso normal, seu imc é: {imc :.2f}")
+elif 25.0<= imc <= 29.9:
+    print(f"sobrepeso, seu imc é: {imc :.2f}")
+elif 30.0 <= imc <= 39.9:
+    print(f"obesidade, seu imc é: {imc :.2f} ")
+else: 
+    print(f"obesidade grave. seu imc é: {imc :.2f}")

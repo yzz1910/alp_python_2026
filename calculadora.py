059fedd11408fc0fe1abc7ec8994d2adacc8af53
yzz1910 <yuusccp@gmail.comgit config --global user.name yzz1910git config --global user.name yuusccp@gmail.com>
2026-04-24 11:28:21 -0300

    mudanças

--- calculadora.py
+++ calculadora.py
@@ -4,19 +4,19 @@ print("2. Subtração")
 print("3. Multiplicação")
 print("4. Divisão")
 
-opcao = int(input("Escolha a operação (1/2/3/4): "))
-num1 = float(input("digite o primeiro número: "))
-num2 = float(input("digite o segundo número: "))
+opcao = float(input("Escolha a operação (1/2/3/4): "))
+if opcao <1 or opcao >4:
+    print("Operação inválida.")
+else:
+    print("Ok, agora digite os números.")
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
 
-if opcao == 1:
-    resultado = num1 + num2
-    print (f"o resultado é {resultado}")
-elif opcao == 2:
-    resultado = num1 - num2
-    print(f"o resultado é {resultado}")
-elif opcao == 3:
-    resultado = num1 * num2
-    print(f"o resultado é {resultado}")
-elif opcao == 4:
-    resultado = num1/num2
-    print(f"o resultado é {resultado}")
+    if opcao == 1:
+        print(f"O resultado da sua operação é: {num1+num2}")
+    elif opcao == 2:
+        print(f"O resultado da sua operação é: {num1-num2}")
+    elif opcao == 3:
+        print(f"O resultado da sua operação é: {num1*num2}")
+    elif opcao == 4:
+        print(f"O resultado da sua operação é: {num1/num2}")

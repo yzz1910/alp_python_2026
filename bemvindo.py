685b93425484644d09061b7b777009dc61b33d17
yzz1910 <yuusccp@gmail.comgit config --global user.name yzz1910git config --global user.name yuusccp@gmail.com>
2026-04-24 10:30:53 -0300

    nnhuma mudança

--- bemvindo.py
+++ bemvindo.py
@@ -1,1 +1,7 @@
-print("bem vindo")
+print("Bem vindo")
+nome ='yuri'
+idade = 16
+print (f"Bem vindo, {nome}")
+print (f"Sua idade é: {idade}")
+
+print("Menu: \n1 Comidas \n2 Bebidas \n3 sair")
---
+++ entradadados.py
@@ -0,0 +1,5 @@
+nome = input ("Digite seu nome: ")
+idade = input ("Digite sua idade: ")
+altura = input ("Digite sua altura: ")
+
+print(f"Bem vindo, {nome} / Sua idade: {idade} / Sua altura: {altura}")

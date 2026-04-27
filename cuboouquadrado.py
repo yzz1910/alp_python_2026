fd703c8638b632ce3bedb0e2b0bcdc3173dc9df8
yzz1910 <yuusccp@gmail.com>
2026-04-27 15:44:04 -0300

    deu problema

---
+++ cuboouquadrado.py
@@ -0,0 +1,5 @@
+numero = int(input("Digite um número:"))
+if numero %2 == 0:
+    print(f"O número é par, seu quadrado é: {numero**2}")
+else:
+    print(f"O número é ímpar, seu cubo é: {numero**3}")
